ce7ef6bf0e0cebb8424a54d40969476f5ee28d23
ctanguy <clemdelatang@gmail.com>
2018-11-27 14:34:24 +0100

    Redirection thanks to State, Redirect component to pseudo message page, message & form components

--- src/Components/Connection.js
+++ src/Components/Connection.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
-
+import { Redirect } from 'react-router-dom'
 
 class Connection extends Component {
+
+  //On crée un state avec un pseudo d'abord vide et une redirection au Chat initialisée à faux
+  state = {
+    pseudo: '',
+    goToChat: false
+  }
+
+  handleChange = event => {
+    const pseudo = event.target.value
+    this.setState({ pseudo })
+  }
+
+  //Gestion forcée du click du formulaire, prise en main de la redirection
+  handleSubmit = event => {
+    event.preventDefault()
+    this.setState({ goToChat: true })
+  }
+
   render() {
+    //Si un pseudo est entré dans le formulaire, puis ensuite validé grâce au bouton, alors seulement redirection vers la template du Chat (avec possibilité  de revenir à la page précédente grâce au props push)
+    if (this.state.goToChat) {
+      return <Redirect push to={`/pseudo/${this.state.pseudo}`} />
+    }
+
     return (
       <div className='connectionBox'>
-      <form className='connection'>
+
+      <form className='connection' onSubmit={ this.handleSubmit }>
+
       <input
+        value={ this.state.pseudo }
+        onChange={ this.handleChange }
         placeholder='Enter your Pseudo'
         type='text'
         required />
+
       <button type='Submit'>Let's Go !</button>
+
       </form>
+
       </div>
     )
   }
---
+++ src/Components/Message.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Message = () => {
+
+  return (
+    <div>
+      <p className="user-message">
+        Mon Message
+      </p>
+    </div>
+  )
+}
+
+
+export default Message
